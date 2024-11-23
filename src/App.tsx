2af01df5c6e2e9362e368df1b1ccf71e9d19ed7d
Zhenyu Zhang <zyzhang_dev@163.com>
2024-11-23 14:31:14 +0000

    config: 支持typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface AppProps {
+  // props
+}
+
+const App: React.FC<AppProps> = ({ }) => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
