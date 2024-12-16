7b5f106b9b677ae83f291f8a3212fb45e6dc3718
Zhenyu Zhang <zyzhang_dev@163.com>
2024-12-16 16:36:38 +0000

    feat: 支持tailwind

--- config-overrides.js
+++ config-overrides.js
@@ -20,10 +20,10 @@ module.exports = override(
         const postcssOptions = postcss.options;
         postcss.options = { postcssOptions };
     }),
-    // addPostcssPlugins([
-    //     require('tailwindcss'),
-    //     require('autoprefixer'),
-    // ]),
+    addPostcssPlugins([
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]),
     // addWebpackModuleRule({
     //     test: /\.module\.less$/,
     //     exclude: /node_modules/,
