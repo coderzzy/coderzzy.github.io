e79d88dda059cdb30ca5098a29868f5e57ff5a6c
Zhenyu Zhang <zyzhang_dev@163.com>
2024-12-17 17:08:31 +0000

    feat: 移动端使用dropdown

--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ColorPicker } from 'antd';
-import { useGlobalContext } from '../context/GlobalContext'
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
+import { Menu, Button, Dropdown } from 'antd';
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { useGlobalContext } from '../context/GlobalContext'
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -25,8 +24,9 @@ const GlobalHeader: React.FC<GlobalHeaderProps> = ({ }) => {
     ];
 
     const { colorTextBase, setColorTextBase } = useGlobalContext();
-    const [current, setCurrent] = useState(items[0]?.key);
     const [windowWidth, setWindowWidth] = useState(0);
+    const [currentMenuKey, setCurrentMenuKey] = useState(items[0]?.key);
+    const [menuCollapsed, setMenuCollapsed] = useState(false);
 
     const navigate = useNavigate();
 
@@ -43,16 +43,31 @@ const GlobalHeader: React.FC<GlobalHeaderProps> = ({ }) => {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
+    const toggedMenuCollapsed = () => {
+        setMenuCollapsed(!menuCollapsed);
+    };
+
     const onMenuClick: MenuProps['onClick'] = (e) => {
-        console.log('click ', e);
-        setCurrent(e.key);
+        setCurrentMenuKey(e.key);
     };
 
     return (
         <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
             <div style={{ fontSize: '1rem', cursor: 'pointer' }} onClick={() => { window.location.href = './' }}>张小刀的杂文</div>
             <div>
-                {windowWidth > 480 && <Menu onClick={onMenuClick} theme='dark' selectedKeys={[current as string]} mode="horizontal" items={items} />}
+                {
+                    windowWidth > 480
+                        ? (<Menu onClick={onMenuClick} theme='dark' selectedKeys={[currentMenuKey as string]} mode="horizontal" items={items} />)
+                        : (
+                            <div>
+                                <Dropdown menu={{ items }}>
+                                    <Button type="primary" onClick={toggedMenuCollapsed} style={{ marginBottom: 16 }}>
+                                        {menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                                    </Button>
+                                </Dropdown>
+                            </div>
+                        )
+                }
             </div>
         </div>
     );
