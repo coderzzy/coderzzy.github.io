1409a7c4d5fedc1636847895450b3915af7f9564
Zhenyu Zhang <zyzhang_dev@163.com>
2024-12-17 16:18:34 +0000

    feat: navigate('/tools')

--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ColorPicker } from 'antd';
 import { useGlobalContext } from '../context/GlobalContext'
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
@@ -7,25 +8,27 @@ import { Menu } from 'antd';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-const items: MenuItem[] = [
-    {
-        key: 'tools',
-        label: (
-            <a href="./tools" target="_blank">
-                小工具
-            </a>
-        ),
-    }
-];
-
 interface GlobalHeaderProps {
     // props
 }
 
 const GlobalHeader: React.FC<GlobalHeaderProps> = ({ }) => {
+    const items: MenuItem[] = [
+        {
+            key: 'tools',
+            label: (
+                <div onClick={() => navigate('/tools')}>
+                    小工具
+                </div>
+            ),
+        }
+    ];
+
     const { colorTextBase, setColorTextBase } = useGlobalContext();
     const [current, setCurrent] = useState(items[0]?.key);
     const [windowWidth, setWindowWidth] = useState(0);
+
+    const navigate = useNavigate();
 
     // 监听窗口大小变化
     useEffect(() => {
@@ -45,10 +48,9 @@ const GlobalHeader: React.FC<GlobalHeaderProps> = ({ }) => {
         setCurrent(e.key);
     };
 
-
     return (
         <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-            <div style={{ fontSize: '1rem', cursor: 'pointer' }} onClick={()=>{window.location.href='./'}}>张小刀的杂文</div>
+            <div style={{ fontSize: '1rem', cursor: 'pointer' }} onClick={() => { window.location.href = './' }}>张小刀的杂文</div>
             <div>
                 {windowWidth > 480 && <Menu onClick={onMenuClick} theme='dark' selectedKeys={[current as string]} mode="horizontal" items={items} />}
             </div>
