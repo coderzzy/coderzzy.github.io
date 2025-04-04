7029309896dc62efac1adc5b4e0cfe4f4bf9573d
zzy <zyzhang_dev@163.com>
2025-04-05 00:17:39 +0800

    feat: 个人详情页进一步优化

--- src/pages/PersonalDetailsPage.tsx
+++ src/pages/PersonalDetailsPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Layout, Row, Col, Image } from 'antd'; // 添加Image导入
+import { Layout, Row, Col, Image, Card, Divider, List, Tooltip } from 'antd';
+import { GithubOutlined, ZhihuOutlined, WechatOutlined, RedEnvelopeOutlined } from '@ant-design/icons';
 import GlobalHeader from '../components/GlobalHeader';
 import styles from './PersonalDetailsPage.module.less';
 
@@ -9,30 +10,32 @@ const PersonalDetailsPage: React.FC = () => {
     return (
         <Layout className={styles.layout}>
             <Header>
-                <GlobalHeader/>
+                <GlobalHeader />
             </Header>
             <Content className={styles.content}>
                 <Row gutter={[24, 24]} justify="center">
                     <Col xs={24} sm={20} md={18} lg={16}>
-                        <div className={styles.card}>
+                        <Card className={styles.card}>
                             <h1 className={styles.title}>个人详情</h1>
                             <h2 className={styles.name}>张小刀</h2>
                             <p className={styles.slogan}>Slogan: 小刀需磨，方才不钝</p>
+                            <Divider />
                             <h3 className={styles.subtitle}>社交媒体账号</h3>
                             <ul className={styles.list}>
                                 <li>
                                     <a href="https://github.com/coderzzy" target="_blank" className={styles.link}>
-                                        Github
+                                        <GithubOutlined /> Github
                                     </a>
                                 </li>
                                 <li>
                                     <a href="https://www.zhihu.com/people/zhang-xiao-yu-41-25" target="_blank" className={styles.link}>
-                                        知乎
+                                        <ZhihuOutlined /> 知乎
                                     </a>
                                 </li>
-                                {/* 修改微信公众号二维码部分 */}
                                 <li className={styles.socialMediaItem}>
-                                    <span className={styles.socialMediaText}>微信公众号：</span>
+                                    <span className={styles.socialMediaText}>
+                                        <WechatOutlined /> 微信公众号：
+                                    </span>
                                     <Image
                                         width={100}
                                         src="/social_media/wechat_official_account.png"
@@ -40,10 +43,10 @@ const PersonalDetailsPage: React.FC = () => {
                                         className={styles.socialMediaQrCode}
                                     />
                                 </li>
-                                
-                                {/* 修改小红书二维码部分 */} 
                                 <li className={styles.socialMediaItem}>
-                                    <span className={styles.socialMediaText}>小红书号：</span>
+                                    <span className={styles.socialMediaText}>
+                                        <RedEnvelopeOutlined /> 小红书号：
+                                    </span>
                                     <Image
                                         width={100}
                                         src="/social_media/rednote_account.jpg"
@@ -52,38 +55,49 @@ const PersonalDetailsPage: React.FC = () => {
                                     />
                                 </li>
                             </ul>
+                            <Divider />
                             <h3 className={styles.subtitle}>独立开发者项目</h3>
-                            <ul className={styles.list}>
-                                <li className={styles.projectItem}>
-                                    飞书多维表格插件 - 分账计算
-                                    <a 
-                                        href="https://v0j3dbddef2.feishu.cn/base/extension/replit_3d519d1a976ee3e5" 
-                                        target="_blank" 
-                                        rel="noopener noreferrer"
-                                        className={styles.link}
-                                    >
-                                        (插件链接)
-                                    </a>
-                                    <p className={styles.projectDesc}>
-                                        基于借贷记账法实现的分账功能，可自动计算多人AA制消费后的最优转账方案
-                                    </p>
-                                </li>
-                                <li className={styles.projectItem}>
-                                    Chrome浏览器插件 - 拼音小助手
-                                    <a 
-                                        href="https://chromewebstore.google.com/detail/hmojmlnloknlieigobgihcbjbflmfmli?utm_source=item-share-cb" 
-                                        target="_blank" 
-                                        rel="noopener noreferrer"
-                                        className={styles.link}
-                                    >
-                                        (插件链接)
-                                    </a>
-                                    <p className={styles.projectDesc}>
-                                        帮助用户快速查询汉字拼音的浏览器插件
-                                    </p>
-                                </li>
-                            </ul>
-                        </div>
+                            <List
+                                dataSource={[
+                                    {
+                                        title: '飞书多维表格插件 - 分账计算',
+                                        link: 'https://v0j3dbddef2.feishu.cn/base/extension/replit_3d519d1a976ee3e5',
+                                        description: '基于借贷记账法实现的分账功能，可自动计算多人AA制消费后的最优转账方案'
+                                    },
+                                    {
+                                        title: 'Chrome浏览器插件 - 拼音小助手',
+                                        link: 'https://chromewebstore.google.com/detail/hmojmlnloknlieigobgihcbjbflmfmli?utm_source=item-share-cb',
+                                        description: '帮助用户快速查询汉字拼音的浏览器插件'
+                                    },
+                                    {
+                                        title: 'Chrome浏览器插件 - 打工人小助手',
+                                        link: 'https://chromewebstore.google.com/detail/elhofejeghpaedmaheibfaenfgbbjddl?utm_source=item-share-cb',
+                                        description: '帮助用户快速查询工作状态的浏览器插件'
+                                    }
+                                ]}
+                                renderItem={(item) => (
+                                    <List.Item>
+                                        <div>
+                                            <Tooltip title={item.description}>
+                                                <span>{item.title}</span>
+                                            </Tooltip>
+                                            <a 
+                                                href={item.link} 
+                                                target="_blank" 
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                                style={{ fontSize: '0.9em' }} // 临时使用内联样式调小字体
+                                            >
+                                                (插件链接)
+                                            </a>
+                                            <p className={styles.projectDesc}>
+                                                {item.description}
+                                            </p>
+                                        </div>
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
                     </Col>
                 </Row>
             </Content>
