e72c2913aa06344ee7beac5606a74b26c8d78d44
zzy <zyzhang_dev@163.com>
2025-04-04 23:14:49 +0800

    feat: 博客页引流到微信公众号

--- src/data/BlogMetaInfo.tsx
+++ src/data/BlogMetaInfo.tsx
@@ -8,29 +8,29 @@ interface MetaInfo {
 
 export const metaInfo: MetaInfo[] = [
     {
-        "title": "“金融数学”学了啥？(上)——金融产品",
-        "picturePath": "/images/background_edinburgh.jpg",
-        "description": "作为上篇，我们将主要集中在金融产品部分。",
-        "firstSentence": "简单来说，金融数学的目标就是用数学模型给金融产品定价，例如从未来的价值...",
-        "contentLink": "https://mp.weixin.qq.com/s/afhhsAX3tJyzBuoEpjdz5A"
+        "title": "大模型，小探索 —— AI 帮写个飞书插件，赚个“零花钱” ^_^",
+        "picturePath": "/images/feishuchajian.jpg",
+        "description": "AI 协同的三种模式: Embedding 嵌入、Copilot 副手 和 Agent 智能体。",
+        "firstSentence": "未来的竞争，是 使用 AI 进行提效 和 对 AI 结果进行价值判断 的能力的竞争...",
+        "contentLink": "https://mp.weixin.qq.com/s/NHRTEBYb29hLnoaiNZPFpg"
+    },
+    {
+        "title": "国考出分了，数据分析一波",
+        "picturePath": "/images/guokaofenxi.jpg",
+        "description": "国考虽然G了，但看看数据分析还是挺有意思的。",
+        "firstSentence": "既然 1 月初出了分，本着大过年的，考都考了的心态，小刀觉得不如来做一波数据分析...",
+        "contentLink": "https://mp.weixin.qq.com/s/WtAjZhUNqF05vEFOqYOgAw"
+    },
+    {
+        "title": "“金融数学”学了啥？(下)——金融模型",
+        "picturePath": "/images/shuishuoriji.jpg",
+        "description": "作为下篇，我们将主要集中在金融模型部分。",
+        "firstSentence": "对于金融模型，主要关注市场假设，即股票如何进行变化；以及在这样的市场假设下，如何进行金融产品的定价。...",
+        "contentLink": "https://mp.weixin.qq.com/s/reIL0yqa0bw1VF0onYwOGA"
     },
     {
         "title": "“金融数学”学了啥？(上)——金融产品",
-        "picturePath": "/images/background_edinburgh.jpg",
-        "description": "作为上篇，我们将主要集中在金融产品部分。",
-        "firstSentence": "简单来说，金融数学的目标就是用数学模型给金融产品定价，例如从未来的价值...",
-        "contentLink": "https://mp.weixin.qq.com/s/afhhsAX3tJyzBuoEpjdz5A"
-    },
-    {
-        "title": "“金融数学”学了啥？(上)——金融产品",
-        "picturePath": "/images/background_edinburgh.jpg",
-        "description": "作为上篇，我们将主要集中在金融产品部分。",
-        "firstSentence": "简单来说，金融数学的目标就是用数学模型给金融产品定价，例如从未来的价值...",
-        "contentLink": "https://mp.weixin.qq.com/s/afhhsAX3tJyzBuoEpjdz5A"
-    },
-    {
-        "title": "“金融数学”学了啥？(上)——金融产品",
-        "picturePath": "/images/background_edinburgh.jpg",
+        "picturePath": "/images/shuishuoriji.jpg",
         "description": "作为上篇，我们将主要集中在金融产品部分。",
         "firstSentence": "简单来说，金融数学的目标就是用数学模型给金融产品定价，例如从未来的价值...",
         "contentLink": "https://mp.weixin.qq.com/s/afhhsAX3tJyzBuoEpjdz5A"
