75a8dba03700c530f2e4f768fabee35670c24b08
zzy <zyzhang_dev@163.com>
2025-04-04 22:23:39 +0800

    feat: 个人详情页使用header

--- src/pages/PersonalDetailsPage.tsx
+++ src/pages/PersonalDetailsPage.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Layout } from 'antd';
+import GlobalHeader from '../components/GlobalHeader';
+import styles from './PersonalDetailsPage.module.less'; // 需要创建对应的样式文件
 
-const PersonalDetails: React.FC = () => {
+const { Header, Content } = Layout;
+
+const PersonalDetailsPage: React.FC = () => {
     return (
-        <div>
-            <h1>个人详情</h1>
-            <p>这里展示更多的个人信息，比如独立开发者项目列表。</p>
-            {/* 这里可以添加独立开发者项目列表 */}
-        </div>
+        <Layout className={styles.layout}>
+            <Header>
+                <GlobalHeader />
+            </Header>
+            <Content className={styles.content}>
+                <div>
+                    <h1>个人详情</h1>
+                    <p>这里展示更多的个人信息，比如独立开发者项目列表。</p>
+                </div>
+            </Content>
+        </Layout>
     );
 };
 
-export default PersonalDetails;
+export default PersonalDetailsPage;
